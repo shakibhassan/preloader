3243dfb37dcb189342d53f620530df839e24d066
Shakib Hassan <hasanshakib74@gmail.com>
2017-07-14 23:32:17 +0600

    Add files via upload

---
+++ active.js
@@ -0,0 +1,20 @@
+(function ($) {
+	"use strict";
+    
+    jQuery(document).ready(function($){
+            // This code is use for isotop masonary.
+        ///////////////////////////////////////////////////////
+    
+        
+  
+        
+        
+        
+    });
+
+    jQuery(window).load(function(){
+        $(".century-preloader").fadeOut(500);
+    });
+   
+
+}(jQuery));
